d7b441de430a6fe2aca295aca30a01b17ee63931
Anderwafe <Anderwafe@yandex.by>
2025-03-03 17:21:27 +0300

    New logging and dynamic array features

--- dynarr.h
+++ dynarr.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <assert.h>
-#include <inttypes.h>
+#include <stdint.h>
 #include <string.h>
 #include <math.h>
 #include "logging.h"
@@ -18,46 +18,79 @@
     size_t capacity;\
 } name
 
+typedef struct {
+    void* elements;
+    size_t count;
+    size_t capacity;
+} void_dynarr;
+
 #define DYNARR_RESIZE(object, newCapacity) do { \
     typeof(object) _object = object; \
-    typeof(newCapacity) _newCapacity = newCapacity;\
-    if(_newCapacity <= _object->capacity) break;
+    size_t _newCapacity = newCapacity;\
+    if(!_object) {\
+        LOGWARN(stdout, "Passed NULL as object");\
+        break;\
+    }\
+    if(_newCapacity <= _object->capacity) break; \
     LOGINFO(stdout, "resizing dynarr object = %xp; newCapacity = %zu", _object, _newCapacity);\
     void* temp = memory_recalloc(_object->elements, sizeof(typeof(*((_object)->elements))), _newCapacity);\
     if(temp == NULL) { \
-        LOGERROR(stderr, errno, "Cannot realloc the elements in dynarr, %d -> %d", _object->capacity, _newCapacity); \
-        exit(EXIT_FAILURE); \
+        LOGWARN(stderr, "Cannot realloc the elements in dynarr (because '%s'), %d -> %d", strerror(errno), _object->capacity, _newCapacity); \
+        break; \
     } \
     (_object)->elements = (typeof((_object)->elements))temp; \
     (_object)->capacity = (_newCapacity); \
-    (_object)->count = (_object)->count;\
 } while(0)
+
+int8_t dynarr_resize(void* object, size_t element_size, size_t new_capacity) {
+    if(!object) {
+        LOGWARN(stdout, "Passed NULL as object");
+        return 0;
+    }
+    
+    void_dynarr* dynarr = (void_dynarr*)object;
+    if(new_capacity <= dynarr->capacity) break;
+    LOGINFO(stdout, "resizing dynarr object = %xp; new capacity = %zu", dynarr, new_capacity);
+    void* temp = memory_recalloc(_object->elements, sizeof(typeof(*((_object)->elements))), _newCapacity);
+
+    if(!temp) {
+        LOGWARN(stderr, "Cannot realloc the elements in dynarr (because '%s'), %d -> %d", strerror(errno), dynarr->capacity, new_capacity); \
+        return 0;
+    }
+
+    return 1;
+}
 
 #define DYNARR_INIT(object) do { \
     typeof(object) _object = object; \
-    LOGINFO(stdout, "init dynarr object = %xp", _object);\
+    LOGINFO(stdout, "init dynarr object");\
+    if(!_object) _object = (typeof(_object))memory_malloc(sizeof(_object));\
     assert((_object)->elements == NULL); \
     DYNARR_RESIZE((_object), DYNARR_INITIAL_CAPACITY); \
     assert((_object)->elements != NULL); \
     (_object)->count = 0; \
-    (_object)->capacity = DYNARR_INITIAL_CAPACITY; \
 } while(0)
 
 #define DYNARR_FREE(object) do {\
     typeof(object) _object = object;\
     LOGINFO(stdout, "freeing dynarr object = %xp", _object);\
     memory_free(_object->elements);\
+    _object->elements = NULL;\
     _object->count = 0;\
     _object->capacity = 0;\
 } while(0)
 
 #define DYNARR_APPEND(object, item) do {\
-    typeof(item) _item = item; \
     typeof(object) _object = object;\
+    typeof(*(_object->elements)) _item = item; \
+    if(_object) {\
+        LOGWARN(stdout, "Passed NULL as object");\
+        break;\
+    }\
     LOGINFO(stdout, "appending new item to dynarr object %xp", _object);\
     if((_object)->count >= (_object)->capacity) {\
-        size_t newCapacity = (_object)->capacity * (_object->count / _object->capacity + 1);\
-        LOGINFO(stdout, "resizing dynarr object %xp from %d to %d", _object, _object->capacity, newCapacity);\
+        size_t newCapacity = (_object)->capacity * 2;\
+        LOGINFO(stdout, "resizing dynarr object %xp from %zu to %zu", _object, _object->capacity, newCapacity);\
         DYNARR_RESIZE(_object, newCapacity);\
     }\
     _object->elements[_object->count++] = _item;\
@@ -65,10 +98,14 @@
 
 #define DYNARR_INSERT(object, item, index) do{\
     typeof(object) _object = object;\
-    typeof(item) _item = item;\
-    typeof(index) _index = index;\
-    LOGINGO(stdout, "inserting new item into %d index %xp's dynarr object", _index, _object);\
-    if(_index + 2 >= _object->capacity) {\
+    typeof(*(_object->elements)) _item = item;\
+    size_t _index = index;\
+    if(_object) {\
+        LOGWARN(stdout, "Passed NULL as object");\
+        break;\
+    }\
+    LOGINGO(stdout, "inserting new item into %zu index %xp's dynarr object", _index, _object);\
+    if(_index >= _object->capacity) {\
         uint32_t multiplier = _index / _object->capacity;\
         uint32_t exponent = (uint32_t)(log2f(multiplier)) + 1;\
         multiplier = (uint32_t)(exp2f(multiplier));\
@@ -78,7 +115,19 @@
     }\
     
 
-#define DYNARR_REMOVE(object, index)
+#define DYNARR_REMOVE(object, index) do {\
+    typeof(object) _object = object;\
+    size_t _index = index;\
+    if(_object) {\
+        LOGWARN(stdout, "Passed NULL as object");\
+        break;\
+    }\
+    if(_index >= _object->count) {\
+        LOGWARN(stdout, "Passed index that bigger than count (%d -> %d)", _index, _object->count);\
+        break;\
+    }\
+    memmove(_object->elements[_index], _object->elements[_index+1], (_object->count--) - _index - 1);\
+} while(0)
 
 #define DYNARR_CLEAR(object) (object)->count = 0
     
